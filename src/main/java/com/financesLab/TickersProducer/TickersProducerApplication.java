632bd9f41152472aff8109d02e201a62e5271e18
Eduardo <eduardocasagrande71@gmail.com>
2024-07-18 02:54:09 -0300

    First commit

---
+++ src/main/java/com/financesLab/TickersProducer/TickersProducerApplication.java
@@ -0,0 +1,13 @@
+package com.financesLab.TickersProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickersProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickersProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financesLab/TickersProducer/TickersProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financesLab.TickersProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickersProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
